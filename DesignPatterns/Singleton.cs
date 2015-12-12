c0ca3a4c851993de4f4c70e25a65fb7b8d4f1801
Maciej Lewiński <maciej.lewinski@mobica.com>
2015-12-12 21:41:20 +0100

    Created Initial Solution with two projects: ConsoleApplication and DesignPatterns. Added Singleton Pattern
    Related Work Items: #10, #11

---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns
+{
+    public class Singleton
+    {
+        #region Private Fields
+
+        private static Singleton _instance;
+
+        #endregion Private Fields
+
+        #region Private Constructors
+
+        private Singleton()
+        {
+        }
+
+        #endregion Private Constructors
+
+        #region Public Methods
+
+        public static Singleton GetInstance()
+        {
+            return _instance = _instance ?? new Singleton();
+        }
+
+        public string SayHi(string name)
+        {
+            return $"Hi {name}";
+        }
+
+        #endregion Public Methods
+    }
+}
