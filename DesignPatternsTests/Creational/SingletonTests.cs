15d88b44507234d8b8b6e355bb75aa62bcd06618
Maciej Lewiński <maciej.lewinski@mobica.com>
2015-12-13 13:00:56 +0100

    Design Patterns test project - simple singleton tests included

---
+++ DesignPatternsTests/Creational/SingletonTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace MLewi.DesignPatterns.Creational.Tests
+{
+    [TestFixture()]
+    public class SingletonTests
+    {
+        [Test()]
+        public void GetInstance_ReturnsNotNullSingleton()
+        {
+            var singleton = Singleton.GetInstance();
+            Assert.That(singleton, Is.Not.Null);
+        }
+        
+        [Test()]
+        public void GetInstance_CallingItTwiceReturnsTheSameObject()
+        {
+            var singleton1 = Singleton.GetInstance();
+            var singleton2 = Singleton.GetInstance();
+            Assert.That(singleton1, Is.EqualTo(singleton2));
+        }
+    }
+}
